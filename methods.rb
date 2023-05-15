2cf31bd17cc6fb5662e1576b0d6b77ca79ce325c
Gideon Mutugi <gideonmutugi24@gmail.com>
2023-05-15 15:02:52 +0300

    done with methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
-# Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+
+
+             #greet maureen, jimmy
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+
+
+
+
+             #greet with default
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+
+
+        # add
+
+        def add(a, b)
+            a + b
+          end
+
+
+          # halve
+
+          def halve(number)
+            return nil unless number.is_a?(Integer)
+          
+            number / 2
+          end
+            
